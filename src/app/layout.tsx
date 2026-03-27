59a29881e3fee74d4438b792eb13632e2c485598
Antoine-Bottin <a.bottin.dev@gmail.com>
2026-03-27 15:46:17 +0100

    remove tailwind and install sass

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,16 +23,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-<<<<<<< HEAD
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
-=======
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
->>>>>>> b462c2bd936ab147482ee4a13fa52a1cad987370
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,19 @@
 import Image from "next/image";
-<<<<<<< HEAD
-import styles from "./page.module.css";
+import "./styles.scss";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
+    <div className="page">
+      <main className="main">
         <Image
-          className={styles.logo}
-=======
-
-export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
->>>>>>> b462c2bd936ab147482ee4a13fa52a1cad987370
+          className="logo"
           src="/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
           priority
         />
-<<<<<<< HEAD
-        <div className={styles.intro}>
+        <div className="intro">
           <h1>To get started, edit the page.tsx file.</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
@@ -32,55 +21,29 @@ export default function Home() {
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               target="_blank"
               rel="noopener noreferrer"
-=======
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
->>>>>>> b462c2bd936ab147482ee4a13fa52a1cad987370
             >
               Templates
             </a>{" "}
             or the{" "}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-<<<<<<< HEAD
               target="_blank"
               rel="noopener noreferrer"
-=======
-              className="font-medium text-zinc-950 dark:text-zinc-50"
->>>>>>> b462c2bd936ab147482ee4a13fa52a1cad987370
             >
               Learning
             </a>{" "}
             center.
           </p>
         </div>
-<<<<<<< HEAD
-        <div className={styles.ctas}>
+        <div className="ctas">
           <a
-            className={styles.primary}
+            className="primary"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-=======
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
->>>>>>> b462c2bd936ab147482ee4a13fa52a1cad987370
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-<<<<<<< HEAD
-              className={styles.logo}
-=======
-              className="dark:invert"
->>>>>>> b462c2bd936ab147482ee4a13fa52a1cad987370
+              className="logo"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={16}
@@ -89,13 +52,8 @@ export default function Home() {
             Deploy Now
           </a>
           <a
-<<<<<<< HEAD
-            className={styles.secondary}
+            className="secondary"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-=======
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
->>>>>>> b462c2bd936ab147482ee4a13fa52a1cad987370
             target="_blank"
             rel="noopener noreferrer"
           >
